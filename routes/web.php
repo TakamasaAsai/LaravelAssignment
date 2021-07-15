1a0426c7aca19a016128f3677207607cbe31b059
Takamasa Asai <takamasa.asai@fullspeedtechnologies.com>
2021-07-15 18:56:22 +0900

    clean up unnecessary files

--- routes/web.php
+++ routes/web.php
@@ -17,32 +17,19 @@ Route::get('/', function () {
 
 Route::get('tests/test', 'TestController@index');
 
-//Route::get('contact/index', 'ContactFormController@index');
 Route::get('message/index', 'MessagesController@index');
 
 //authを入れれば、認証してから表示する
-Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function (){
-    Route::get('index', 'ContactFormController@index')->name('contact.index');
-    Route::get('create', 'ContactFormController@create')->name('contact.create');
-    Route::post('store', 'ContactFormController@store')->name('contact.store');
-    Route::get('show/{id}', 'ContactFormController@show')->name('contact.show');
-    Route::get('edit/{id}', 'ContactFormController@edit')->name('contact.edit');
-    Route::post('update/{id}', 'ContactFormController@update')->name('contact.update');
-    Route::post('destroy/{id}', 'ContactFormController@destroy')->name('contact.destroy');
-});
 
 Route::group(['prefix' => 'message', 'middleware' => 'auth'], function (){
     Route::get('index', 'MessagesController@index')->name('message.index');
     Route::get('create', 'MessagesController@create')->name('message.create');
     Route::post('store', 'MessagesController@store')->name('message.store');
-//    Route::get('show/{id}', 'MessagesController@show')->name('message.show');
     Route::get('edit/{id}', 'MessagesController@edit')->name('message.edit');
     Route::post('update/{id}', 'MessagesController@update')->name('message.update');
     Route::post('destroy/{id}', 'MessagesController@destroy')->name('message.destroy');
 });
 
-//REST
-//Route::resource('contacts', 'ContactFormController');
 
 
 Auth::routes();
