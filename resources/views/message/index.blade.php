b6d010527f68d4ebd9386a6561539418dc5c2faa
Takamasa Asai <takamasa.asai@fullspeedtechnologies.com>
2021-07-15 17:29:26 +0900

    modified index.blade.php

--- resources/views/message/index.blade.php
+++ resources/views/message/index.blade.php
@@ -47,59 +47,6 @@
                                 </table>
                             </div>
                         @endforeach
-                        {{--                        <table class="table">--}}
-                        {{--                            <thead>--}}
-                        {{--                            <tr>--}}
-                        {{--                                <th scope="col">id</th>--}}
-                        {{--                                <th scope="col">件名</th>--}}
-                        {{--                                <th scope="col">本文</th>--}}
-                        {{--                                <th scope="col">登録日時</th>--}}
-                        {{--                                <th scope="col"></th>--}}
-
-                        {{--                            </tr>--}}
-                        {{--                            </thead>--}}
-                        {{--                            <tbody>--}}
-
-                        {{--                            @foreach($messages as $message)--}}
-                        {{--                                <tr>--}}
-                        {{--                                    <th>{{ $message->id }}</th>--}}
-                        {{--                                    <td>{{ $message->title }}</td>--}}
-                        {{--                                    <td>{{ $message->message }}</td>--}}
-                        {{--                                    <td>{{ $message->created_at }}</td>--}}
-                        {{--                                    <td>--}}
-                        {{--                                        <form method="GET" action="{{ route('message.edit', ['id' => $message->id])}}">--}}
-                        {{--                                            @csrf--}}
-
-                        {{--                                            <input class="btn btn-info" type="submit" value="変更する">--}}
-                        {{--                                        </form>--}}
-                        {{--                                        <form method="POST"--}}
-                        {{--                                              action="{{ route('message.destroy', ['id' => $message->id])}}"--}}
-                        {{--                                              id="delete{{ $message->id }}">--}}
-                        {{--                                            @csrf--}}
-                        {{--                                            <input class="btn btn-danger" type="submit" onclick="deletePost(this);" value="削除する">--}}
-                        {{--                                        </form>--}}
-                        {{--                                    </td>--}}
-                        {{--                                </tr>--}}
-                        {{--                                <div class="border my-2 p-2">--}}
-                        {{--                                    <div class="text-secondary">{{ $message->title }}</div>--}}
-                        {{--                                    <div class="p-2">{{ $message->message }}</div>--}}
-                        {{--                                    <div class="text-secondary">投稿日:{{ $message->created_at }}</div>--}}
-                        {{--                                    <div><form method="GET" action="{{ route('message.edit', ['id' => $message->id])}}">--}}
-                        {{--                                        @csrf--}}
-
-                        {{--                                        <input class="btn btn-info" type="submit" value="変更する">--}}
-                        {{--                                    </form></div>--}}
-                        {{--                                    <div><form method="POST"--}}
-                        {{--                                          action="{{ route('message.destroy', ['id' => $message->id])}}"--}}
-                        {{--                                          id="delete{{ $message->id }}">--}}
-                        {{--                                        @csrf--}}
-                        {{--                                        <input class="btn btn-danger" type="submit" onclick="deletePost(this);" value="削除する">--}}
-                        {{--                                    </form></div>--}}
-                        {{--                                </div>--}}
-                        {{--                            @endforeach--}}
-                        {{--                            </tbody>--}}
-                        {{--                        </table>--}}
-                        {{--                        {{ $messages->links() }}--}}
                     </div>
                 </div>
             </div>
