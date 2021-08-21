0c336649e64f25d365731237cccbf32ed232c947
Takamasa Asai <takamasa.asai@fullspeedtechnologies.com>
2021-08-21 15:28:36 +0900

    add construct on repository

--- app/Models/Messages.php
+++ app/Models/Messages.php
@@ -3,18 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class Messages extends Model
 {
+        //ModelではDBへの接続のみを行う
+        //何も書かなくてもDBへの接続ができている
 
-    public static function getMessages()
-    {
-        //クエリビルダ
-        return $messages = DB::table('messages')
-            ->select('id', 'title', 'message', 'created_at')
-            ->orderBy('id', 'desc')
-            ->get();
-    }
 
 }
--- app/Repositories/MessagesRepository.php
+++ app/Repositories/MessagesRepository.php
@@ -7,9 +7,21 @@ use App\Models\Messages;
 
 class MessagesRepository
 {
+//    コンストラクタの追加、コンストラクタでnewする
+    private $messages;
+    public function __construct(Messages $messages)
+    {
+        $this->messages = $messages;
+    }
     public function all()
     {
-        return $messages = Messages::getMessages();
+//        Repositoryではクエリの処理のみを書く
+//        \Facades\DBはRepositoryで使わない
+        $messages = $this->messages;
+        return $messages
+        ->select('id', 'title', 'message', 'created_at')
+        ->orderBy('id', 'desc')
+        ->get();
     }
 
 }
